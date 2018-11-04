f41b83ff1714bcd8b533eda7c35b92a8e6b32577
Hassan Sani <saniyhassan@gmail.com>
2018-11-04 19:51:08 +0100

    Mock display user email

--- src/app/components/top-menu/top-menu.component.spec.ts
+++ src/app/components/top-menu/top-menu.component.spec.ts
@@ -1,6 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
+import { NgbRootModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { TopMenuComponent } from './top-menu.component';
+import { ShareService } from 'src/app/services/share.service';
 
 describe('TopMenuComponent', () => {
   let component: TopMenuComponent;
@@ -8,7 +13,8 @@ describe('TopMenuComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TopMenuComponent ]
+      declarations: [ TopMenuComponent ],
+      imports: [ NgbRootModule, RouterModule, RouterTestingModule, FormsModule ]
     })
     .compileComponents();
   }));
@@ -19,7 +25,28 @@ describe('TopMenuComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create Top Menu', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain the Nav bar text Front End Challenge', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.navbar-brand').textContent).toContain('Front End Challenge');
+  });
+
+  it('should have user email', inject([ShareService], (service: ShareService) => {
+    expect(service).toBeTruthy();
+    let userEmail = component.userEmail;
+    const mockObj = {
+      email: 'something@another.yes',
+      password: '12345678',
+      confirmPassword: '12345678'
+    };
+    service.registerUser(mockObj);
+
+    service.userData.subscribe(result => userEmail = result.email);
+    expect(userEmail).toContain('something@another.yes');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.userEmail').textContent).toContain('something@another.yes');
+  }));
 });
--- src/app/components/top-menu/top-menu.component.ts
+++ src/app/components/top-menu/top-menu.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ShareService } from 'src/app/services/share.service';
+import { User } from 'src/app/interface/User';
 
 @Component({
   selector: 'app-top-menu',
   templateUrl: './top-menu.component.html',
-  styles: []
+  styles: [`
+  select.custom-select {
+    margin: 0.5rem 0.5rem 0 0;
+    width: auto;
+  }
+`]
 })
 export class TopMenuComponent implements OnInit {
 
-  constructor() { }
+
+  userEmail: string;
+  displayMonths = 2;
+  navigation = 'select';
+  showWeekNumbers = false;
+  outsideDays = 'visible';
+
+  constructor(
+    private mockUser: ShareService,
+  ) {}
 
   ngOnInit() {
+    this.mockUser.userData.subscribe((result: User) => {
+      this.userEmail = result.email;
+    });
   }
-
 }
